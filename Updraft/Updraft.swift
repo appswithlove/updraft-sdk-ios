5b5cb0a2c274246f1d2ca06e6781ce22a2bd1e6c
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2018-01-22 13:33:56 +0100

    Basic Updraft initialization

--- Updraft/Updraft.swift
+++ Updraft/Updraft.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+public class Updraft {
+	
+	private init() {}
+	private static let sharedInstance = Updraft()
+	
+	private(set) var appKey = ""
+	
+	/// Returns the shared Updraft instance.
+	open class var shared: Updraft {
+		return sharedInstance
+	}
+	
+	/// Clears Updraft configs
+	public func clear() {
+		appKey = ""
+	}
+	
+	/// Starts Updraft with your appKey.
+	/// This method should be called after the app is launched and before using Updraft services.
+	///
+	/// - Parameter appKey: Your application key
+	public func start(with appKey: String) {
+		self.appKey = appKey
+	}
+}
--- UpdraftTests/UpdraftTests.swift
+++ UpdraftTests/UpdraftTests.swift
@@ -10,29 +10,42 @@ import XCTest
 @testable import Updraft
 
 class UpdraftTests: XCTestCase {
+	
+	var updraft: Updraft!
     
     override func setUp() {
         super.setUp()
 		
-		
         // Put setup code here. This method is called before the invocation of each test method in the class.
+		updraft = Updraft.shared
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+		updraft.clear()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+	
+	func testStartUpdraft() {
+		//Given
+		let appKey = "123456789"
+		
+		//When
+		updraft.start(with: appKey)
+		
+		//Then
+		XCTAssertEqual(appKey, updraft.appKey)
+	}
+	
+	func testClearConfig() {
+		//Given
+		let appKey = "123456789"
+		updraft.start(with: appKey)
+		
+		//When
+		updraft.clear()
+		
+		//Then
+		XCTAssertEqual(updraft.appKey, "")
+	}
 }
