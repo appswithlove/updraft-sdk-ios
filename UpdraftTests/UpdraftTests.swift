2a9990ac444b4d8bc53b9b1f2da93971659cda21
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2018-01-22 10:52:47 +0100

    style

--- UpdraftTests/UpdraftTests.swift
+++ UpdraftTests/UpdraftTests.swift
@@ -14,6 +14,7 @@ class UpdraftTests: XCTestCase {
     override func setUp() {
         super.setUp()
 		
+		
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
