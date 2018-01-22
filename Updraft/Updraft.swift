2b67d8f37d933bbcce6ae3a6f2eb5c9edf33f575
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2018-01-22 12:04:51 +0100

    update gitignore

---
+++ Updraft/Updraft.swift
@@ -0,0 +1,9 @@
+//
+//  Updraft.swift
+//  Updraft
+//
+//  Created by Raphael Neuenschwander on 22.01.18.
+//  Copyright © 2018 Raphael Neuenschwander. All rights reserved.
+//
+
+import Foundation
