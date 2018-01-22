d6ae243cc8d88db7514ec9c9826cb66020b6218b
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2018-01-22 10:29:37 +0100

    first commit

---
+++ Updraft/Updraft.h
@@ -0,0 +1,19 @@
+//
+//  Updraft.h
+//  Updraft
+//
+//  Created by Raphael Neuenschwander on 22.01.18.
+//  Copyright © 2018 Raphael Neuenschwander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Updraft.
+FOUNDATION_EXPORT double UpdraftVersionNumber;
+
+//! Project version string for Updraft.
+FOUNDATION_EXPORT const unsigned char UpdraftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Updraft/PublicHeader.h>
+
+
