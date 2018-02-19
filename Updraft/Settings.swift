c09cf7239c18b955785a715b69dbff95b831ca33
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2018-02-19 12:21:57 +0100

    Add basic settings

---
+++ Updraft/Settings.swift
@@ -0,0 +1,33 @@
+//
+//  Settings.swift
+//  Updraft
+//
+//  Created by Raphael Neuenschwander on 19.02.18.
+//  Copyright © 2018 Apps with love AG. All rights reserved.
+//
+
+import Foundation
+
+/// Settings of the sdk
+class Settings {
+	var sdkKey: String
+	var appKey: String
+	
+	init(sdkKey: String, appKey: String) {
+		self.sdkKey = sdkKey
+		self.appKey = appKey
+	}
+	
+	convenience init() {
+		self.init(sdkKey: "", appKey: "")
+	}
+}
+
+// MARK: - Clearable
+
+extension Settings: Clearable {
+	func clear() {
+		sdkKey = ""
+		appKey = ""
+	}
+}
