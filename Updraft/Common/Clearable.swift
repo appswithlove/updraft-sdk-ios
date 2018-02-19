1f01b6867af3daede47bd744a3a5704694d73ac8
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2018-02-19 12:21:28 +0100

    Add clearable protocol

---
+++ Updraft/Common/Clearable.swift
@@ -0,0 +1,14 @@
+//
+//  Clearable.swift
+//  Updraft
+//
+//  Created by Raphael Neuenschwander on 19.02.18.
+//  Copyright © 2018 Apps with love AG. All rights reserved.
+//
+
+import Foundation
+
+//Simple clear protocol
+protocol Clearable {
+	func clear()
+}
