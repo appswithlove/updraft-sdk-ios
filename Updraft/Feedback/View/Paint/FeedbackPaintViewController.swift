52b44603347b796707e94e52a56919f7ea3c4713
Raphael Neuenschwander <raphael@appswithlove.com>
2020-10-02 11:31:42 +0200

    Fix background image set in feedback overlay

--- Updraft/Feedback/View/Paint/FeedbackPaintViewController.swift
+++ Updraft/Feedback/View/Paint/FeedbackPaintViewController.swift
@@ -35,7 +35,8 @@ class FeedbackPaintViewController: UIViewController {
 			return drawViewController.editedImage
 		}
 		set {
-			drawViewController.backgroundImage = image
+//			drawViewController.backgroundImage = image
+			drawViewController.backgroundImage = newValue
 		}
 	}
 	
